5ffa58e30577902477c59a150b54c0dcb7fdf90e
gdegroot <glenn@iconum.com>
2013-07-22 16:39:20 +0200

    Update IiDealHttpRequest.cs

--- src/iDeal/Http/IiDealHttpRequest.cs
+++ src/iDeal/Http/IiDealHttpRequest.cs
@@ -1,10 +1,11 @@
-﻿using iDeal.Base;
+using iDeal.Base;
 using iDeal.SignatureProviders;
+using System;
 
 namespace iDeal.Http
 {
     public interface IiDealHttpRequest
     {
-        iDealResponse SendRequest(iDealRequest idealRequest, ISignatureProvider signatureProvider, string url, IiDealHttpResponseHandler iDealHttpResponseHandler);
+        iDealResponse SendRequest(iDealRequest idealRequest, ISignatureProvider signatureProvider, string url, IiDealHttpResponseHandler iDealHttpResponseHandler, ref iDealException exception);
     }
 }
