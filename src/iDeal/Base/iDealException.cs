2ae2abb69349ba7c64bcf34b96f4273052776a63
gdegroot <glenn@iconum.com>
2013-07-22 16:25:28 +0200

    Update iDealException.cs

--- src/iDeal/Base/iDealException.cs
+++ src/iDeal/Base/iDealException.cs
@@ -1,21 +1,28 @@
-﻿using System;
+using System;
 using System.Xml.Linq;
 
 namespace iDeal.Base
 {
-    public class iDealException : Exception
+    public class iDealException : SystemException
     {
         public DateTime CreateDateTimeStamp { get; private set; }
         public string ErrorCode { get; private set; }
         public string ErrorMessage { get; private set; }
         public string ErrorDetail { get; private set; }
         public string ConsumerMessage { get; set; }
-        
+        public string ErrorDescriptionCombined
+        {
+            get
+            {
+                return "Code: " + ErrorCode + ", Message: " + ErrorMessage + ", Detail: " + ErrorDetail;
+            }
+        }
+
         public iDealException(XElement xDocument)
         {
-            XNamespace xmlNamespace = "http://www.idealdesk.com/Message";
+            XNamespace xmlNamespace = "http://www.idealdesk.com/ideal/messages/mer-acq/3.3.1";
 
-            CreateDateTimeStamp = DateTime.Parse(xDocument.Element(xmlNamespace + "createDateTimeStamp").Value);
+            CreateDateTimeStamp = DateTime.Parse(xDocument.Element(xmlNamespace + "createDateTimestamp").Value);
 
             ErrorCode = xDocument.Element(xmlNamespace + "Error").Element(xmlNamespace + "errorCode").Value;
             ErrorMessage = xDocument.Element(xmlNamespace + "Error").Element(xmlNamespace + "errorMessage").Value;
